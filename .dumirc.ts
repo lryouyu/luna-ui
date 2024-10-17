96a0e548d4ba2c3be0f606e59cf558a9051234e1
rui.liu <3311579771@qq.com>
2024-10-17 13:36:19 +0800

    feat: dist upload

--- .dumirc.ts
+++ .dumirc.ts
@@ -6,9 +6,6 @@ export default defineConfig({
     name: 'luna-ui',
   },
   logo: false,
-<<<<<<< HEAD
   base: '/luna-ui/', // GitHub Pages 路径前缀（仓库名）
   publicPath: '/luna-ui/', // 静态资源路径前缀
-=======
->>>>>>> cb52dee61b1c755ae41f40eb3d26afabb2ae85cd
 });
